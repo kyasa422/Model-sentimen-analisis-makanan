aee8eedbba2e8814c6ffabb481a1f89539f5ac01
Agus Setiawan <kyasa422@gmail.com>
2024-06-24 18:32:28 +0700

    masih eror

--- app.py
+++ app.py
@@ -18,13 +18,18 @@ def home() -> Any:
     return {"message": "Sentiment Analysis API is running!"}
 
 @app.post("/predict")
-def predict(data: dict) -> Any:
-    if not data.text:
-        raise HTTPException(status_code=400, detail="No text provided")
+# def predict(data: TextData) -> Any:
+#     if not data.text:
+#         raise HTTPException(status_code=400, detail="No text provided")
 
-    text = data.text
-    X = vectorizer.transform([text])
-    prediction = model.predict(X)
+#     text = data.text
+#     X = vectorizer.transform([text])
+#     prediction = model.predict(X)
+def predict(data: dict):
+    text = data['text']
+    text_vector = vectorizer.transform([text])
+    prediction = model.predict(text_vector)
+    # return {"prediction": prediction[0]}
     # Convert numpy.int64 to int
     sentiment = "positive" if prediction[0] == 1 else "negative"
 
