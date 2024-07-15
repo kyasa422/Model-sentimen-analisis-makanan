9dd1827068094e908ff390410b5e2cb152153fec
Agus Setiawan <kyasa422@gmail.com>
2024-07-15 15:47:57 +0700

    akhirnya revisi lagi

--- app.py
+++ app.py
@@ -1,53 +1,7 @@
-# from fastapi import FastAPI, HTTPException
-# from pydantic import BaseModel
-# from typing import Any
-# import joblib
-# import numpy as np
-
-# app = FastAPI()
-
-# # Load model and vectorizer
-# model = joblib.load('model_sentimen_analis2.pkl')
-# vectorizer = joblib.load('vectorizer2.pkl')
-
-# class PredictRequest(BaseModel):
-#     text: str
-
-# @app.get("/")
-# def home() -> Any:
-#     return {"message": "Sentiment Analysis API is running!"}
-
-# @app.post("/predict")
-# # def predict(data: TextData) -> Any:
-# #     if not data.text:
-# #         raise HTTPException(status_code=400, detail="No text provided")
-
-# #     text = data.text
-# #     X = vectorizer.transform([text])
-# #     prediction = model.predict(X)
-# # def predict(data: dict):
-# #     text = data['text']
-# #     text_vector = vectorizer.transform([text])
-# #     prediction = model.predict(text_vector)
-#     # return {"prediction": prediction[0]}
-#     # Convert numpy.int64 to int
-    
-# def predict(request: PredictRequest):
-#     print(request.text)
-#     text = request.text
-#     text_vector = vectorizer.transform([text])
-#     prediction = model.predict(text_vector)
-#     sentiment = "positive" if prediction[0] == 1 else "negative"
-
-#     return {"prediction": sentiment}
-
-# if __name__ == '__main__':
-#     import uvicorn
-#     uvicorn.run(app)
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
-import requests
+from nltk.stem import PorterStemmer
 
 
 class PredictRequest(BaseModel):
@@ -56,8 +10,8 @@ class PredictRequest(BaseModel):
 app = FastAPI()
 
 # Load your model and vectorizer using joblib
-model = joblib.load("model_sentimen_analis2.pkl")
-vectorizer = joblib.load("vectorizer2.pkl")
+model = joblib.load("model_sentimen_analis5.pkl")
+vectorizer = joblib.load("tfidf_vectorizer.pkl")
 
 @app.get("/")
 def read_root():
@@ -65,20 +19,29 @@ def read_root():
     return {"message": "Sentiment Analysis API is running!"}
 
 
-@app.route("/predict", methods=['POST'])
-@app.post("/predict" )
+
+@app.post("/predict")
 def predict(request: PredictRequest):
+    
+    # Preprocessing teks baru (stemming)
+    def stem_text(text):
+        stemmer = PorterStemmer()
+        return ' '.join([stemmer.stem(word) for word in text.split()])
+    
     text = request.text
-    text_vector = vectorizer.transform([text])
+    new_text = stem_text(text)
+    text_vector = vectorizer.transform([new_text])
     prediction = model.predict(text_vector)
-    sentiment = "positive" if prediction[0] == 1 else "negative"
+    if prediction == 1:
+        sentiment = "Positif"
+    elif prediction == -1:
+        sentiment = "Negatif"
+    else:
+        sentiment = "Netral"
+        
+    # return {"prediction": prediction}
     return {"prediction": sentiment}
    
-
-
-
-
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+   return {"prediction": sentiment}
